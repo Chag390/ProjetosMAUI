368f519fd82d4c01826ec4a3f0c0f52a683a5946
debor <debor@DESKTOP-84A7417>
2023-08-18 09:06:23 -0300

    Adicionar arquivos de projeto.

---
+++ AppContentPage/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppContentPage
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppNumeroDaSorte/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppNumeroDaSorte;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//currentPage = PaginaAtual
+		MainPage = new MainPage();
+	}
+}
